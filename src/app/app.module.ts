970ddc66e0d16a9490bdc1c1695f8812e81fba38
joebercoutinho <125513833+JoeberCoutinho@users.noreply.github.com>
2023-12-23 13:52:27 -0300

    primeiro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DadosPessoaisComponent } from './componentes/dados-pessoais/dados-pessoais.component';
+import { ResumoProfissionalComponent } from './componentes/resumo-profissional/resumo-profissional.component';
+import { FormacaoAcademicaComponent } from './componentes/formacao-academica/formacao-academica.component';
+import { ExperienciaProfissionalComponent } from './componentes/experiencia-profissional/experiencia-profissional.component';
+import { CompetenciasComponent } from './componentes/competencias/competencias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DadosPessoaisComponent,
+    ResumoProfissionalComponent,
+    FormacaoAcademicaComponent,
+    ExperienciaProfissionalComponent,
+    CompetenciasComponent
   ],
   imports: [
     BrowserModule
